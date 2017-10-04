ac49da2f56a714c39cc185ed4a79d0c4a34028d8
KManu <kobbykmanu7@gmail.com>
2017-10-04 12:34:15 +0000

    Fix

--- server.js
+++ server.js
@@ -1,37 +1,34 @@
 // load the express package and create our app
 var express = require('express');
-var app     = express();
+var app = express();
 
 // set the port based on environment (more on environments later)
-var port    = 1337; 
+var port = 1337;
 
-app.use(express.static(__dirname + '/js'))
+//Serve the site folder 
+app.use(express.static(__dirname + '/site'))
 
-// send our index.html file to the user for the home page
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-});
 
-	// create routes for the admin section
+// create routes for the admin section
 
-	// get an instance of the router
-	var adminRouter = express.Router();
+// get an instance of the router
+var adminRouter = express.Router();
 
 
 
-	// route middleware that will happen on every request
-	adminRouter.use(function(req, res, next) {
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
 
 	// log each request to the console
 	console.log(req.method, req.url);
 
 	// continue doing what we were doing and go to the route
-	next(); 
-	});
+	next();
+});
 
 
-	// route middleware to validate :name
-	adminRouter.param('name', function(req, res, next, name) {
+// route middleware to validate :name
+adminRouter.param('name', function(req, res, next, name) {
 	// do validation on name here
 	// blah blah validation
 	// log something so we know its working
@@ -39,33 +36,33 @@ app.get('/', function(req, res) {
 
 	// once validation is done save the new item in the req
 	req.name = name;
-	next(); 
-	});
+	next();
+});
 
-	// route with parameters (http://localhost:1337/admin/hello/:name)
-	adminRouter.get('/hello/:name', function(req, res) {
+// route with parameters (http://localhost:1337/admin/hello/:name)
+adminRouter.get('/hello/:name', function(req, res) {
 	res.send('hello ' + req.name + '!');
-	});
+});
 
 
 
-	// admin main page. the dashboard (http://localhost:1337/admin)
-	adminRouter.get('/', function(req, res) {
+// admin main page. the dashboard (http://localhost:1337/admin)
+adminRouter.get('/', function(req, res) {
 	res.send('I am the dashboard!');
-	});
+});
 
-	// users page (http://localhost:1337/admin/users)
-	adminRouter.get('/users', function(req, res) {
+// users page (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req, res) {
 	res.send('I show all the users!');
-	});
+});
 
-	// posts page (http://localhost:1337/admin/posts)
-	adminRouter.get('/posts', function(req, res) {
+// posts page (http://localhost:1337/admin/posts)
+adminRouter.get('/posts', function(req, res) {
 	res.send('I show all the posts!');
-	});
+});
 
-	// apply the routes to our application
-	app.use('/admin', adminRouter);
+// apply the routes to our application
+app.use('/admin', adminRouter);
 
 // start the server
 app.listen(1337);
